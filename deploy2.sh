c4b52c61a9201f4b1b87997c3361349fae244940
Rustam Kuramshin <kuramshin.py@yandex.ru>
2024-06-11 22:34:13 +0300

    Запуск в docker-compose.yaml

--- deploy2.sh
+++ deploy2.sh
@@ -13,16 +13,16 @@ git checkout feature/no-ref/rkuramshin
 git pull
 
 # Шаг 4: Остановка всех работающих контейнеров
-docker compose down
+docker-compose down
 
 # Шаг 5: Удаление всех образов из docker-compose
-docker compose rm -f
+docker-compose rm -f
 docker image prune -f --filter "label=com.docker.compose.project=nakormi"
 
 # Шаг 6: Сборка проекта nakormi
 ./gradlew -Pvaadin.productionMode=true bootJar
 
 # Шаг 7: Запуск всех сервисов
-docker compose up -d
+docker-compose up -d
 
 echo "Deployment completed successfully"
